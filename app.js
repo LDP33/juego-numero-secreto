74c6c2d1d5617996f5943035787ee31581a6d7eb
leandropollano <97859191+LDP33@users.noreply.github.com>
2025-02-17 15:33:50 -0300

    version final: número secreto alura

---
+++ app.js
@@ -0,0 +1,57 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let numerosSorteados = [];
+let numeroMaximo = 10;
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento("h1","Juego de adivinanza");
+    asignarTextoElemento("p","Indica un número del 1 al 10.");
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1
+}
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+    console.log(numeroSecreto);
+    console.log(numeroSecreto == numeroDeUsuario);
+    if (numeroSecreto === numeroDeUsuario) {
+        asignarTextoElemento("p",`Acertaste el número en ${intentos} ${(intentos === 1) ? "intento" : "intentos"}!`);
+        document.getElementById("reiniciar").removeAttribute("disabled");
+    } else {
+        if (numeroDeUsuario < numeroSecreto) {
+            asignarTextoElemento("p","El número secreto es mayor");
+    } else { asignarTextoElemento("p","El número secreto es menor");}    
+    } 
+    intentos++;
+    limpiarCaja();
+} 
+
+function limpiarCaja() {
+    document.querySelector("#valorUsuario").value = "";
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
+
+    if (numerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento("p","Ya se sortearon todos los numeros posibles.")
+        } else {
+            if (numerosSorteados.includes(numeroGenerado)) {
+                return generarNumeroSecreto();
+            }  else {
+                numerosSorteados.push(numeroGenerado);
+                return numeroGenerado;
+                }
+    }  
+}
+
+function reiniciarJuego() {
+    limpiarCaja();
+    condicionesIniciales();
+    document.getElementById("reiniciar").setAttribute("disabled", true);
+}
+
+condicionesIniciales();
